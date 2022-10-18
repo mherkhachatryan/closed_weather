198defe300db8953ab7fda8057dd4eaa82048fcb
Armine <arminekhtfa@gmail.com>
2022-10-18 21:08:58 +0400

    I added some part of front

--- web/front_page.py
+++ web/front_page.py
@@ -1,3 +1,14 @@
 import streamlit as st
 
 st.title('Closed Weather')
+
+st.header("Please enter your coordinates")
+
+longitude = st.number_input('long', value=-0.1)
+latitude = st.number_input('lat',value=-0.1)
+#print(longitude, latitude)
+if (longitude == True) and (latitude == True):
+    st.text("We are showing requested data")
+#st.text("We are showing requested data")
+#st.balloons()
+#print(longitude, latitude)
