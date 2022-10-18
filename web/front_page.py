1daa868f98e28a3300f5e198a19d9effd19551a7
mherkhachatryan <mherkhachatryan35912@gmail.com>
2022-10-18 21:57:46 +0400

    added front and back integration

--- web/front_page.py
+++ web/front_page.py
@@ -1,5 +1,10 @@
 import streamlit as st
 
+import sys
+
+sys.path.append("../closed_weather/")
+from closed_weather.weather import get_weather
+
 st.title('Closed Weather')
 
 st.header("Please enter your coordinates")
@@ -8,4 +13,7 @@ longitude = st.number_input('long', value=-0.1)
 latitude = st.number_input('lat', value=-0.1)
 
 if longitude >= 0 and latitude >= 0:
-    st.text("We are showing requested data")
+    weather = get_weather(longitude, latitude)
+
+    st.dataframe(weather)
+
